5ed02555d79a884a07cf7ba5ac734f93827bbdbe
Matthew Turner <matthew.m.turner@outlook.com>
2024-12-13 10:11:44 -0500

    Update gitignore

--- crates/fetch/target/debug/deps/fetch-53e81b969c87acb9.d
+++
@@ -1,5 +0,0 @@
-/Users/matth/projects/rfsee/crates/fetch/target/debug/deps/fetch-53e81b969c87acb9: src/lib.rs
-
-/Users/matth/projects/rfsee/crates/fetch/target/debug/deps/fetch-53e81b969c87acb9.d: src/lib.rs
-
-src/lib.rs:
--- crates/fetch/target/debug/deps/fetch-c941f21c39502f92.d
+++
@@ -1,9 +0,0 @@
-/Users/matth/projects/rfsee/crates/fetch/target/debug/deps/libfetch-c941f21c39502f92.rmeta: src/lib.rs Cargo.toml
-
-/Users/matth/projects/rfsee/crates/fetch/target/debug/deps/fetch-c941f21c39502f92.d: src/lib.rs Cargo.toml
-
-src/lib.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
