cf67370c4521dbbbaa477a83e18b54e83fd206ec
Matthew Turner <matthew.m.turner@outlook.com>
2024-12-29 23:10:20 -0500

    Update lua test

--- tests/search_index.lua
+++ tests/search_index.lua
@@ -18,9 +18,8 @@ struct RfcSearchResults* search_terms(const char* terms);
 ]])
 
 local current_dir = lfs.currentdir();
-
-local dylib = current_dir .. "/target/debug/libffi.so"
-print("Lib path: ", dylib)
+local lib_extension = (jit.os == "OSX") and ".dylib" or ".so"
+local dylib = current_dir .. "/artifacts/libffi" .. lib_extension
 local lib = ffi.load(dylib)
 
 local results = lib.search_terms("Hello")
@@ -33,8 +32,3 @@ for i = 0, results.len - 1 do
     local title = ffi.string(rfc.title):gsub("\n", " ")
     print("Title: ", title)
 end
-
-
--- if results == nil or results.error ~= 0 then
---     error("Error searching: ", results.error)
--- end
